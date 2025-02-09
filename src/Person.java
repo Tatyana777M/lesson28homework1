c2fa1592415f2efad2cb999f52be58dac7334b23
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-09 20:43:23 +0100

    Class Person finished

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public static int comparePersons(Person p1, Person p2) {
+        return p1.age - p2.age;
+    }
+    @Override
+    public String toString() {
+        return name + " (" + age + " лет)";
+    }
+    public static void bubbleSort(Person[] people) {
+        int a = people.length;
+        boolean swapped;
+        for (int i = 0; i < a - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < a - 1 - i; j++) {
+                if (Person.comparePersons(people[j], people[j + 1]) > 0) {
+                    Person temp = people[j];
+                    people[j] = people[j + 1];
+                    people[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
