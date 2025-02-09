289a7acb0edd4335116c3616f0f1ba0d05a107be
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-09 20:55:41 +0100

    Class Main edited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-
+        Person[] people = {
+                new Person("Jack", 45),
+                new Person("Ann", 35),
+                new Person("John", 20),
+                new Person("Mary", 25),
+        };
+        System.out.println("До сортировки:");
+        for (Person p : people) {
+            System.out.println(p);
+        }
+        Person.bubbleSort(people);
+        System.out.println("\n");
+        System.out.println("После сортировки:");
+        for (Person p : people) {
+            System.out.println(p);
+        }
     }
 }
+
+
+
+
+
+
+
+
